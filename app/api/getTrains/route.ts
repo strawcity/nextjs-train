f6296d5da95f1e4e8dd58d33c8a2b46a485e3a9b
Erik Nilsson <erik@strawcitydesign.com>
2023-06-24 20:55:49 +0200

    Adding data cache revalidation

--- app/api/getTrains/route.ts
+++ app/api/getTrains/route.ts
@@ -1,14 +1,13 @@
-import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const trains = await axios
-    .get(
-      `https://api.sl.se/api2/realtimedeparturesV4.json?key=${process.env.API_KEY}&siteid=9261&timewindow=0&bus=false
-`
-    )
-    .then((response) => {
-      return response.data.ResponseData;
-    });
-  return NextResponse.json(trains);
+  const trains = await fetch(
+    `https://api.sl.se/api2/realtimedeparturesV4.json?key=${process.env.API_KEY}&siteid=9261&timewindow=0&bus=false
+`,
+    { next: { revalidate: 60 } }
+  ).then((response) => {
+    return response;
+  });
+  const data = await trains.json();
+  return NextResponse.json(data.ResponseData);
 }
